801f574a29e84a1da3ea73419c149deb9694d467
MelanySecaira <nayeli.secaira@espoch.edu.ec>
2023-11-27 21:26:12 -0500

    vinculando a db_endosolutions

--- servidor/src/database/datebase.js
+++ servidor/src/database/datebase.js
@@ -1,6 +1,6 @@
 import Sequelize from 'sequelize';
 
-export const sequelize = new Sequelize("db_selecpan", "postgres", "secaira", {
+export const sequelize = new Sequelize("db_endosolutions", "postgres", "secaira", {
     host: "localhost",
     dialect: "postgres",
 })
